db556891ddfb16d1c3dbd175927aebf5d6455eb9
Ruslan Tyan <rtyan@plesk.com>
2022-05-18 00:35:00 +0700

    fixup!

--- vars/TestIterate.groovy
+++ vars/TestIterate.groovy
@@ -3,5 +3,11 @@
 def call(list1, list2) {
     println list1.toString()
     println list2.toString()
+    list1.each {
+        println it
+        list2.each {
+            println it
+        }
+    }
     return true
 }
